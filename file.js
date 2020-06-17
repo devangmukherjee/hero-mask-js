0a6ef7fdd917f8ba1db386505770ebcd8d6eb96e
devangmukherjee <devangmukherjee@gmail.com>
2020-06-17 20:54:41 +0530

    upload button mask staying problem

---
+++ file.js
@@ -0,0 +1,56 @@
+var loadFile = async() => {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+    await faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+    
+    const canvas = faceapi.createCanvasFromMedia(image);
+    const context = canvas.getContext('2d');
+    console.log(context)
+
+    
+    const detection = await faceapi.detectAllFaces(canvas, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
+  
+    const rightEye=detection[0].landmarks.getRightEye()
+    const leftEye=detection[0].landmarks.getLeftEye()
+  
+    const right_x_avg=(rightEye[0].x+rightEye[1].x+rightEye[3].x+rightEye[4].x+rightEye[5].x+rightEye[2].x)/6
+    const right_y_avg=(rightEye[0].y+rightEye[1].y+rightEye[3].y+rightEye[4].y+rightEye[5].y+rightEye[2].y)/6
+    const left_x_avg=(leftEye[0].x+leftEye[1].x+leftEye[3].x+leftEye[4].x+leftEye[5].x+leftEye[2].x)/6
+    const left_y_avg=(leftEye[0].y+leftEye[1].y+leftEye[3].y+leftEye[4].y+leftEye[5].y+leftEye[2].y)/6
+    
+    const leftEyeCentroid = [left_x_avg, left_y_avg]
+    const rightEyeCentroid = [right_x_avg, right_y_avg]
+    const eyesCentroid = [(leftEyeCentroid[0] + rightEyeCentroid[0])/2, (leftEyeCentroid[1] + rightEyeCentroid[1])/2]
+  
+    const eyedist = Math.sqrt(Math.pow(right_x_avg-left_x_avg, 2) + Math.pow(right_y_avg-left_y_avg, 2))
+  
+    const mask_by_eye=0.45
+  
+    const width = eyedist/mask_by_eye
+    const height = width * 480 / 1280
+    const newOffsetTop = eyesCentroid[1] - height/2
+    const newOffsetLeft = eyesCentroid[0] - width/2
+    
+    const angleEyes=Math.atan((left_y_avg-right_y_avg)/(left_x_avg-right_x_avg)) * 180 / Math.PI
+    
+    const item = document.querySelector(".grid-item")
+    
+    const overlay = document.createElement("img")
+    overlay.src = "./mask.png"
+    overlay.alt = "mask overlay."
+    overlay.style.cssText = `
+      position: absolute;
+      left: ${newOffsetLeft}px;
+      top: ${newOffsetTop+50}px;
+      width: ${eyedist/mask_by_eye}px;
+      transform: rotate(${angleEyes}deg);
+      z-index:1;
+    `
+  
+    item.appendChild(overlay);    
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(context)
+
+};
+
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const landmark = async() => {
-  //await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
   await faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
   await faceapi.nets.faceExpressionNet.loadFromUri('/models');
@@ -10,21 +9,7 @@ const landmark = async() => {
 
   const rightEye=detection[0].landmarks.getRightEye()
   const leftEye=detection[0].landmarks.getLeftEye()
-  const jawline=detection[0].landmarks.getJawOutline()
-  const eyebrow=detection[0].landmarks.getLeftEyeBrow()
-  const nose = detection[0].landmarks.getNose()
 
-  const leftEyeMaskCentroid = [158,118]
-  const rightEyeMaskCentroid = [454,118]
-  
-  const maskEyesCentroid = [(leftEyeMaskCentroid[0] + rightEyeMaskCentroid[0])/2, 118]
-
-  const jawLeft = jawline[0]
-  const jawRight = jawline[16]
-  const adjacent = jawRight.x - jawLeft.x
-  const opposite = jawRight.y - jawLeft.y
-  const jawLength = Math.sqrt(Math.pow(adjacent, 2) + Math.pow(opposite, 2))
-  const jawangle=(opposite/adjacent)*100
   const right_x_avg=(rightEye[0].x+rightEye[1].x+rightEye[3].x+rightEye[4].x+rightEye[5].x+rightEye[2].x)/6
   const right_y_avg=(rightEye[0].y+rightEye[1].y+rightEye[3].y+rightEye[4].y+rightEye[5].y+rightEye[2].y)/6
   const left_x_avg=(leftEye[0].x+leftEye[1].x+leftEye[3].x+leftEye[4].x+leftEye[5].x+leftEye[2].x)/6
@@ -32,56 +17,32 @@ const landmark = async() => {
   
   const leftEyeCentroid = [left_x_avg, left_y_avg]
   const rightEyeCentroid = [right_x_avg, right_y_avg]
-
   const eyesCentroid = [(leftEyeCentroid[0] + rightEyeCentroid[0])/2, (leftEyeCentroid[1] + rightEyeCentroid[1])/2]
 
-
-  
-  const dist = right_x_avg-left_x_avg
   const eyedist = Math.sqrt(Math.pow(right_x_avg-left_x_avg, 2) + Math.pow(right_y_avg-left_y_avg, 2))
 
-
   const mask_by_eye=0.45
-  const eyebrow_by_eye=0.467
-  const leftMask_by_leftEye=0.25
-
-  const h=(left_y_avg-eyebrow[2].y)/eyebrow_by_eye
 
   const width = eyedist/mask_by_eye
   const height = width * 480 / 1280
   const newOffsetTop = eyesCentroid[1] - height/2
   const newOffsetLeft = eyesCentroid[0] - width/2
   
-  
   const angleEyes=Math.atan((left_y_avg-right_y_avg)/(left_x_avg-right_x_avg)) * 180 / Math.PI
-  const leftOffset= left_x_avg-(leftMask_by_leftEye*(eyedist/mask_by_eye));
-  const topOffset= eyebrow[2].y;
-
-  const dimensions = {
-    width: image.width,
-    height: image.height
-};
-
-  const resizedDimensions = faceapi.resizeResults(detection, dimensions);
-  document.body.append(canvas);
-
-  faceapi.draw.drawFaceLandmarks(canvas, resizedDimensions);
  
   const item = document.querySelector(".grid-item")
 
-
   const overlay = document.createElement("img") 
-  overlay.src = document.querySelector(".mask").src;
+  overlay.src = "./mask.png"
   overlay.alt = "mask overlay."
   overlay.style.cssText = `
     position: absolute;
     left: ${newOffsetLeft}px;
-    top: ${newOffsetTop}px;
+    top: ${newOffsetTop+77}px;
     width: ${eyedist/mask_by_eye}px;
     transform: rotate(${angleEyes}deg);
     z-index:1;
   `
-  //console.log(document.querySelector(".mask"))
 
   item.appendChild(overlay)
 }
